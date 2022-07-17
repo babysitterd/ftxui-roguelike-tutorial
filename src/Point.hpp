0deffc66388a40aa3adcdc5aa2c20d4e37783d31
babysitterd <levon.dev@gmail.com>
2022-07-17 13:34:37 +0100

    WASM build fixes

--- src/Point.hpp
+++ src/Point.hpp
@@ -8,4 +8,14 @@ struct Point
     int y = 0;
     // Whoaa SPACESHIP is here!
     auto operator<=>(Point const&) const = default;
+
+    Point(int _x, int _y) : x(_x), y(_y)
+    {
+    }
+
+    Point(Point const&) = default;
+    Point& operator=(Point const&) = default;
+
+    Point(Point&&) = default;
+    Point& operator=(Point&&) = default;
 };
